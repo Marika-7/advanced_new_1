585dfb9714342c7f2a217120ef83f59b5966d9d5
Marika-7 <raduga.dva@gmail.com>
2021-12-06 05:59:56 +0200

    Advanced 1

---
+++ src/app/cenzor/cenzor.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cenzor',
+  templateUrl: './cenzor.component.html',
+  styleUrls: ['./cenzor.component.scss']
+})
+export class CenzorComponent implements OnInit {
+
+  public arrBadList = ['java', 'tottenham'];
+  public isEmpty1 = false;
+  public setBadWord = '';
+  public placeholderInput = 'word here..';
+  public isEmpty2 = false;
+  public textField = '';
+  public placeholderArea = 'text here..';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  showBadList(): string {
+    return this.arrBadList.join(', ');
+  }
+
+  addToBadList(): void {
+    if(this.setBadWord) {
+      this.arrBadList.push(this.setBadWord.toLowerCase());
+      this.clearInput();
+    }
+    else {
+      this.isEmpty1 = true;
+      this.placeholderInput = 'Please write a word!';
+    }
+  }
+
+  resetBadList(): void {
+    this.arrBadList = [];
+    this.clearInput();
+    this.clearArea();
+  }
+
+  clearInput():void {
+    this.isEmpty1 = false;
+    this.placeholderInput = 'word here..';
+    this.setBadWord = '';
+  }
+
+  clearArea():void {
+    this.isEmpty2 = false;
+    this.placeholderArea = 'text here..';
+    this.textField = '';
+  }
+
+  checkTextField(): void {
+    if(this.textField) {
+      this.editText();
+    }
+    else {
+      this.isEmpty2 = true;
+      this.placeholderArea = 'Please write a text!';
+    }
+  }
+
+  editText(): void {
+    let text = this.textField.split(' ');
+    this.arrBadList.map(bad => {
+      text = text.map(word => (word.toLowerCase() === bad) 
+        ? word = word.split('').map(letter => letter = '*').join('') 
+        : word)
+    })
+    this.textField = text.join(' ');
+  }
+  
+}
